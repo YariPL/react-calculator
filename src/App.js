1bcb8a3712b6fc8bac111562777acc43dcfc6409
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-26 22:59:27 +0100

    created basic of structure

--- src/App.js
+++ src/App.js
@@ -28,6 +28,41 @@ class Buttons extends Component {
   render() {
     return(
       <div className="Buttons">
+        <div class='row'>
+
+        </div>
+        <div class='row'>
+          <div class='ac'>
+          </div>
+          <div class='by'>
+          </div>
+          <div class='times'>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='seven'>
+          </div>
+          <div class='eight'>
+          </div>
+          <div class='nine'>
+          </div>
+          <div class='minus'>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='four'>
+          </div>
+          <div class='five'>
+          </div>
+          <div class='six'>
+          </div>
+          <div class='plus'>
+          </div>
+        </div>
+        <div class='row'>
+        
+        </div>
+         
       </div>
     )
   }
