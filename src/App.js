4f773542cc9a368cebf7601b044b1fa04d855736
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-27 22:33:02 +0100

    added buttons text in span elements

--- src/App.js
+++ src/App.js
@@ -30,47 +30,64 @@ class Buttons extends Component {
       <div className="Buttons">
         <div className='row'>
           <div className='ac'>
+          <span>AC</span>
           </div>
           <div className='by'>
+          <span>/</span>
           </div>
           <div className='times'>
+          <span>X</span>
           </div>
         </div>
         <div className='row'>
           <div className='seven'>
+          <span>7</span>
           </div>
           <div className='eight'>
+          <span>8</span>
           </div>
           <div className='nine'>
+          <span>9</span>
           </div>
           <div className='minus'>
+          <span>-</span>
           </div>
         </div>
         <div className='row'>
           <div className='four'>
+          <span>4</span>
           </div>
           <div className='five'>
+          <span>5</span>
           </div>
           <div className='six'>
+          <span>6</span>
           </div>
           <div className='plus'>
+          <span>+</span>
           </div>
         </div>
         <div className='row rowSpecial equalRow'>
           <div className='one'>
+          <span>1</span>
           </div>
           <div className='two'>
+          <span>2</span>
           </div>
           <div className='three'>
+          <span>3</span>
           </div>
-         <div className='equal'>
+          <div className='equal'>
+          <span>=</span>
           </div>
         </div>
 
         <div className='row rowSpecial'>
           <div className='zero'>
+          <span>0</span>
           </div>
           <div className='dot'>
+          <span>.</span>
           </div>
         </div>
          
