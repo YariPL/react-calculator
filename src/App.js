733007f97a9f98c7d9f5ed20f77fe3a6e00d982d
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-28 21:25:44 +0100

    added state for value + display value is Display class

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+     value:0
+    }
+  }
+  calculate =function(e){
+    console.log(e);
+  }
   render() {
     return (
       <div className="App">
-        <Display />
-        <Buttons />
+        <Display value={this.state.value}/>
+        <Buttons calculate={this.calculate}/>
       </div>
     );
   }
@@ -18,85 +27,80 @@ class Display extends Component {
   render() {
     return(
       <div className="Display">
-
+        <div>{this.props.value}</div>
       </div>
     )
   }
 }
 
 class Buttons extends Component {
-  constructor() {
-    super();
-    this.state = {
 
-    }
-  }
 
-  calculate =function(e){
-    console.log(e);
+  handleClick = function(e) {
+    this.props.calculate(e);
   }
   render() {
     return(
       <div className="Buttons">
         <div className='row'>
-          <div className='ac' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='ac' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>AC</span>
           </div>
-          <div className='by' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='by' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>/</span>
           </div>
-          <div className='times' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='times' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>X</span>
           </div>
         </div>
         <div className='row'>
-          <div className='seven' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='seven' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>7</span>
           </div>
-          <div className='eight' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='eight' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>8</span>
           </div>
-          <div className='nine' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='nine' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>9</span>
           </div>
-          <div className='minus' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='minus' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>-</span>
           </div>
         </div>
         <div className='row'>
-          <div className='four' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='four' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>4</span>
           </div>
-          <div className='five' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='five' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>5</span>
           </div>
-          <div className='six' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='six' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>6</span>
           </div>
-          <div className='plus' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='plus' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>+</span>
           </div>
         </div>
         <div className='row rowSpecial equalRow'>
-          <div className='one' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='one' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>1</span>
           </div>
-          <div className='two' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='two' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>2</span>
           </div>
-          <div className='three' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='three' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>3</span>
           </div>
-          <div className='equal' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='equal' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>=</span>
           </div>
         </div>
 
         <div className='row rowSpecial'>
-          <div className='zero' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='zero' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>0</span>
           </div>
-          <div className='dot' onClick={(event)=>this.calculate(event.target.className)}>
+          <div className='dot' onClick={(event)=>this.handleClick(event.target.className)}>
           <span>.</span>
           </div>
         </div>
